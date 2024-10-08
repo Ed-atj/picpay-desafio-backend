b2d85ceff750f932ac5553f5260aa043e4eae4f0
Ed <Edmilsonjunior075@gmail.com>
2024-10-08 06:31:52 -0300

    Changes in 'GlobalExceptionHandler'.
    Changed 'GlobalExceptionHandler' to 'RestExceptionHandler'.
    refactor: Moved to controller package for better management and clarity.
    feat: Methods to handle 'ArgumentNotValidException' and 'TransferSenderNotFoundException'.

---
+++ src/main/java/com/picpay/desafio/domain/controller/RestExceptionHandler.java
@@ -0,0 +1,44 @@
+package com.picpay.desafio.domain.controller;
+
+import com.picpay.desafio.domain.exception.BaseApplicationException;
+import com.picpay.desafio.domain.exception.TransferSenderNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class RestExceptionHandler {
+
+    @ExceptionHandler(BaseApplicationException.class)
+    public ProblemDetail handleBaseApplicationException(BaseApplicationException e) {
+        return e.problemDetail();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ProblemDetail handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+
+        var fieldErros = e.getFieldErrors()
+                .stream()
+                .map(f -> new InvalidParam(f.getField(), f.getDefaultMessage()))
+                .toList();
+
+        var pb = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
+
+        pb.setTitle("Your request parameters didn't validate.");
+        pb.setProperty("invalid-params", fieldErros);
+
+        return pb;
+    }
+
+    @ExceptionHandler(TransferSenderNotFoundException.class)
+    public ProblemDetail handleTransferSenderNotFound(TransferSenderNotFoundException e) {
+        var problemDetail = ProblemDetail.forStatus(HttpStatus.NOT_FOUND);
+        problemDetail.setTitle("Transfer sender not found");
+        problemDetail.setDetail(e.getMessage());
+        return problemDetail;
+    }
+
+    private record InvalidParam(String name, String reason) {}
+}
